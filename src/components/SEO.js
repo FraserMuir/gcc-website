5691a18e179f973ca9223f65526eadf085ae15ba
FraserMuir <frasermuir14@gmail.com>
2021-01-25 18:35:26 +0000

    test fonts for ie

--- src/components/SEO.js
+++ src/components/SEO.js
@@ -37,7 +37,10 @@ export const SEO = () => {
       <meta name="twitter:description" content={description} />
 
       <link rel="preconnect" href="https://fonts.gstatic.com" />
-      <link href="https://fonts.googleapis.com/css2?family=Libre+Baskerville&family=Open+Sans&family=Sacramento&display=swap" rel="stylesheet" />
+      <link href="https://fonts.googleapis.com/css2?family=Libre+Baskerville&display=swap" rel="stylesheet" />
+      <link href="https://fonts.googleapis.com/css2?family=Open+Sans&display=swap" rel="stylesheet" />
+      <link href="https://fonts.googleapis.com/css2?family=Sacramento&display=swap" rel="stylesheet" />
+
     </Helmet>
   );
 };
