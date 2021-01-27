75d47f8ac57dad9ec7078081409874dd5c419287
FraserMuir <frasermuir14@gmail.com>
2021-01-27 16:17:03 +0000

    Fix home import

--- src/data/IndexPagePreview.js
+++ src/data/IndexPagePreview.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { frontmatterWrapper } from "../helpers/frontmatterWrapper";
-import IndexPage from "../app/Home";
+import IndexPage from "../app/home";
 
 export const IndexPagePreview = ({ entry, getAsset }) => {
   const data = entry.getIn(["data"]).toJS();
