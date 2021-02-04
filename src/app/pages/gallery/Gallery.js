18f4555713db8ec6b48d1eaede904ee1c4bce8d5
FraserMuir <frasermuir14@gmail.com>
2021-02-04 21:54:57 +0000

    push new pictures

--- src/app/pages/gallery/Gallery.js
+++ src/app/pages/gallery/Gallery.js
@@ -1,12 +1,40 @@
 import React from "react";
 import { Layout } from "components/Layout";
+import styled from "styled-components";
+import { Image } from "components/Image";
 
 export const Gallery = ({ frontmatter, preview }) => {
-  const { image } = frontmatter || {};
+  const { image, photos } = frontmatter || {};
 
   return (
     <Layout preview={preview} image={image}>
-      <h1>Gallery</h1>
+      <StyledGallery>
+        {(photos || []).map((photo, i) => (
+          <Photo key={i} {...photo} />
+        ))}
+      </StyledGallery>
     </Layout>
   );
 };
+
+const StyledGallery = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+`;
+
+const Photo = ({ photo, caption, date, credit }) => {
+  if (!photo) return null;
+  return (
+    <StyledPhoto>
+      <Image imageData={photo} />
+      {caption && <p>{caption}</p>}
+      {date && <p className="date">{date}</p>}
+      {credit && <p className="credit">{credit}</p>}
+    </StyledPhoto>
+  );
+};
+
+const StyledPhoto = styled.div`
+  display: flex;
+  flex-flow: column nowrap;
+`;
