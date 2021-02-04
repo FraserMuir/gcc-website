504779e5aba10693504261b08151c4614a6c7c4a
FraserMuir <frasermuir14@gmail.com>
2021-02-04 01:01:43 +0000

    add historical meetings

--- src/app/pages/historical-meetings/HistoricalMeetings.js
+++ src/app/pages/historical-meetings/HistoricalMeetings.js
@@ -1,12 +1,13 @@
 import React from "react";
 import { Layout } from "components/Layout";
+import { HistoricalMeetingsWidget } from "app/widgets/historical-meetings";
 
 export const HistoricalMeetings = ({ frontmatter, preview }) => {
   const { image } = frontmatter || {};
 
   return (
     <Layout preview={preview} image={image}>
-      <h1>HistoricalMeetings</h1>
+      {!preview && <HistoricalMeetingsWidget />}
     </Layout>
   );
 };
--- src/app/widgets/historical-meetings/index.md
+++ src/app/widgets/historical-meetings/index.md
@@ -1,6 +1,3 @@
 ---
 type: widget
-meetings:
-  - date: 2021-02-01T17:00:24.809Z
-    file: /media/reporters.png
 ---
