7da0c27eb10deb44992e53981ac9e5de0443debf
FraserMuir <frasermuir14@gmail.com>
2021-02-05 17:22:29 +0000

    Fix meeting

--- src/helpers/getPublicUrl.js
+++ src/helpers/getPublicUrl.js
@@ -1,6 +1,6 @@
 export const getPublicUrl = (item = {}) => {
   if (item.childImageSharp) return item;
   return {
-    url: item.publicURL || `${window.location.origin}/media/${item.path.split("/").slice(-1)[0]}`,
+    url: item.publicURL || `${window.location.origin}/media/${item.path?.split("/")?.slice(-1)?.[0]}`,
   };
 };
